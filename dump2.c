aa574e211cdc8719f5eb615e00a0324329008c57
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2026-01-23 08:40:38 +0100

    new case

--- dump2.c
+++ dump2.c
@@ -284,6 +284,7 @@ int value32_valid(const uint32_t value32)
         || value32 == 0x1
         || value32 == 0x100
         || value32 == 0x200
+        || value32 == 0x300
         || value32 == 0x10000
         || value32 == 0x10001
         || value32 == 0x1000000
